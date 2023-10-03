a132c74453e75df2a98df019b29757459463f40e
ghanekarparnika <parnikaghanekar@gmail.com>
2023-10-03 21:36:41 +0530

    Given a Message, ability to analyse and respond Happy or Sad Mood

---
+++ ExceptionDemo/MoodAnalyser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    public class MoodAnalyser
+    {
+        private string message;
+       public MoodAnalyser(string message) 
+        {
+            this.message = message;
+        }
+
+        public string analyseMood()
+        {
+            try
+                {
+                if (this.message.Contains("sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }catch (Exception ) 
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
